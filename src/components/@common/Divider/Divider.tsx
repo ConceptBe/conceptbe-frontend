adb68184011e1e3c0d80904c3d1ab4d005a3228c
semnil5202 <semnil5202@gmail.com>
2024-01-02 23:44:46 +0900

    refactor: Divider 컴포넌트 Default Type 명 수정

--- src/components/@common/Divider/Divider.tsx
+++ src/components/@common/Divider/Divider.tsx
@@ -1,14 +1,14 @@
 import { Wrapper } from './Divider.style';
 import { ColorKeyType } from '../../../styles/theme';
 
-interface IDivider {
+interface Props {
   top?: number;
   bottom?: number;
   color?: ColorKeyType;
   height?: number;
 }
 
-const Divider = ({ top = 0, bottom = 0, color = 'b', height = 1 }: IDivider) => {
+const Divider = ({ top = 0, bottom = 0, color = 'b', height = 1 }: Props) => {
   return <Wrapper top={top} bottom={bottom} divideColor={color} height={height} />;
 };
 
