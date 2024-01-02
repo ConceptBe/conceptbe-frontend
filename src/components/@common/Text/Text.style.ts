cb3b91bc694f1027734efde8b1eb8a3bba7a90aa
semnil5202 <semnil5202@gmail.com>
2024-01-02 23:54:57 +0900

    fix: Text 컴포넌트 font size 미적용 오류 수정

--- src/components/@common/Text/Text.style.ts
+++ src/components/@common/Text/Text.style.ts
@@ -7,7 +7,7 @@ export const TextWrapper = styled.span<{ textColor: ColorKeyType; textFont: Font
   user-select: none;
   flex-direction: row;
   color: ${({ theme, textColor }) => theme.color[textColor]};
-  font-size: ${({ theme, textFont }) => theme.font[textFont].fontSize};
+  font-size: ${({ theme, textFont }) => theme.font[textFont].fontSize}px;
   font-weight: ${({ theme, textFont }) => theme.font[textFont].fontWeight};
 `;
 
