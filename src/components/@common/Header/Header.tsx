00e38db21d6b2f6437e31f875af5c0fabea7d8e9
semnil5202 <semnil5202@gmail.com>
2024-01-05 03:23:12 +0900

    refactor: HeaderProps 명 Props로 변경

--- src/components/@common/Header/Header.tsx
+++ src/components/@common/Header/Header.tsx
@@ -4,13 +4,13 @@ import { ReactNode } from 'react';
 import HeaderItem from './HeaderItem';
 import Spacer from '../Spacer/Spacer';
 
-interface HeaderProps {
+interface Props {
   children: ReactNode;
   spacerPosition?: 'start' | 'end';
   main?: boolean;
 }
 
-const Header = ({ children, main, spacerPosition }: HeaderProps) => {
+const Header = ({ children, main, spacerPosition }: Props) => {
   return (
     <Container main={main}>
       {spacerPosition === 'start' && <Spacer size={24} />}
