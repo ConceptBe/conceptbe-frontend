57c4ace617422c3a23d15bf2faed2bbfb9314a36
Youngjin Park <youngjin988@gmail.com>
2024-01-21 23:11:18 +0900

    feat: 피드 내부 각 섹션 `Skeleton` 추가

---
+++ src/pages/feed/components/BestIdeaCardListSectionSkeleton.tsx
@@ -0,0 +1,21 @@
+import { Spacer } from 'concept-be-design-system';
+
+import { CardListWrapper, Wrapper } from './BestIdeaCardListSection';
+import BestIdeaCardSkeleton from '../../../components/Card/BestIdeaCardSkeleton';
+import Skeleton from '../../../components/Skeleton/Skeleton';
+
+const BestIdeaCardListSectionSkeleton = () => {
+  return (
+    <Wrapper>
+      <Skeleton width="45%" height="22px" />
+      <Spacer size={18} />
+      <CardListWrapper>
+        {Array.from({ length: 5 }).map((_, idx) => (
+          <BestIdeaCardSkeleton key={idx} />
+        ))}
+      </CardListWrapper>
+    </Wrapper>
+  );
+};
+
+export default BestIdeaCardListSectionSkeleton;
