d26123a68ffca4159df4486f2067e6fe2676bb4c
yogjin <youngjin988@gmail.com>
2024-01-30 22:48:37 +0900

    feat: 글 작성 post api header에 auth 추가 및 성공 시 피드로 라우팅

--- src/pages/write/hooks/mutations/usePostIdeasMutation.ts
+++ src/pages/write/hooks/mutations/usePostIdeasMutation.ts
@@ -2,18 +2,25 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { http } from '../../../../api/http';
 import { PostIdeasRequest } from '../../types';
+import { useNavigate } from 'react-router-dom';
 
 const _postIdeas = (ideas: PostIdeasRequest) => {
-  return http.post('/ideas', ideas);
+  return http.post('/ideas', ideas, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+    },
+  });
 };
 
 export const usePostIdeasMutation = () => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
 
   const { mutate: postIdeas, ...rest } = useMutation({
     mutationFn: _postIdeas,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['ideas'] });
+      navigate('/');
     },
   });
 
