e1236fa65040e4e8d196d6336d61a6fd9cbe7c07
yogjin <youngjin988@gmail.com>
2024-01-30 22:48:50 +0900

    feat: 글 작성 임시 에러핸들링 추가

--- src/pages/write/hooks/mutations/usePostIdeasMutation.ts
+++ src/pages/write/hooks/mutations/usePostIdeasMutation.ts
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { http } from '../../../../api/http';
 import { PostIdeasRequest } from '../../types';
 import { useNavigate } from 'react-router-dom';
+import { AxiosError } from 'axios';
 
 const _postIdeas = (ideas: PostIdeasRequest) => {
   return http.post('/ideas', ideas, {
@@ -11,6 +12,9 @@ const _postIdeas = (ideas: PostIdeasRequest) => {
     },
   });
 };
+
+// TODO: 제대로 된 에러핸들링 추가(토스트 추가 등)
+type PostIdeaError = AxiosError<{ message: string }>;
 
 export const usePostIdeasMutation = () => {
   const navigate = useNavigate();
@@ -21,6 +25,9 @@ export const usePostIdeasMutation = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['ideas'] });
       navigate('/');
+    },
+    onError: (error: PostIdeaError) => {
+      alert(error.response?.data.message ?? '글 작성에 실패했습니다.');
     },
   });
 
