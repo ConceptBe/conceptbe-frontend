a2deec19d83836005bd1503de1ab4c575493e524
이세민 <89172499+semnil5202@users.noreply.github.com>
2024-01-31 21:10:36 +0900

    Feature/#24: 게시글 상세 페이지 구현 (#27)
    * feat: 게시물 상세 받아오는 axios fetch 함수 추가
    
    * feat: useGetFeedDetail Query 생성 및 데이터 가져오는 기능 구현
    
    Date 데이터에 대한 명세가 정확하지 않다. 따라서 임시로 string으로 대체한다.
    
    * refactor: FeedDetail 페이지 코로케이션 적용 및 일부 데이터 서버에서 가져온 값으로 매핑
    
    * refactor: ProfileInfo API 통해 가져온 값으로 매핑
    
    현재 목데이터로는 UI가 망가지는데 해당 데이터들은 실제로는 존재하지 않을수가 없는 콜드케이스이다. (입력하지 않으면 회원가입자체가 안되므로). 따라서 UI가 망가지는 현상을 알고 있으나 추후 실제 데이터로 보완하도록한다.
    
    * refactor: ProfileInfo 공통컴포넌트 적용 및 default Image url 적용
    
    추후 이미지가 없을 경우 default image url을 지정하여 만일의 오류 상태를 대비한다.
    
    * refactor: 댓글 및 대댓글 영역 컴포넌트 분리
    
    * design: FeedDetailPage 디자인 시안과 다른 부분 일괄 수정
    
    * feat: 수정하기 드롭다운 외부 영역 클릭시 드롭다운 닫는 기능 추가
    
    * design: hr 태그 기본 마진값으로 인한 디자인 수정 및 reset css 속성 수정
    
    * refactor: 페이지 윗단에 suspense 적용
    
    * feat: 게시글 카테고리 분야 정보로 임시 대표
    
    게시글 카테고리에 대한 기획 명세가 없어 분야 리스트를 /로 구별하여 임시로 표현한다.
    
    * refactor: TODO 주석 추가
    
    * refactor: params undefined 타입 as키워드로 제한

---
+++ src/pages/FeedDetail/components/Comments.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import { Spacer, theme } from 'concept-be-design-system';
+
+import Comment from './Comment';
+import { CommentParentResponse } from '../types';
+
+interface Props {
+  comments: CommentParentResponse[];
+}
+
+const Comments = ({ comments }: Props) => (
+  <CommentWrapper>
+    <InputBox>
+      <Input placeholder="댓글을 입력해 주세요." />
+    </InputBox>
+    <Spacer size={20} />
+    {comments.map((comment, idx) => (
+      <Comment key={idx} comment={comment} />
+    ))}
+  </CommentWrapper>
+);
+
+export default Comments;
+
+const CommentWrapper = styled.div`
+  padding: 20px 22px 20px 22px;
+`;
+
+const InputBox = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
+  border-radius: 6px;
+  width: 100%;
+  padding: 10px 20px;
+  box-sizing: border-box;
+  border: none;
+  background-color: ${theme.color.bg1};
+  color: ${theme.color.t};
+  font-style: normal;
+  font-family: SUIT;
+  font-weight: 400;
+  line-height: normal;
+  ::placeholder {
+    color: ${theme.color.ba};
+  }
+`;
