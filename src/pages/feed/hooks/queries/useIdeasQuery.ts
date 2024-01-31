f5da3383faf60449628b9421691c77e00a5f6e0c
Youngjin Park <youngjin988@gmail.com>
2024-01-31 16:28:21 +0900

    fix: 무한스크롤용 페이지네이션 api page 초기값을 0으로 수정

--- src/pages/feed/hooks/queries/useIdeasQuery.ts
+++ src/pages/feed/hooks/queries/useIdeasQuery.ts
@@ -16,13 +16,13 @@ export const useIdeasQuery = () => {
   const sizePerPage = 20; // 한 페이지에 보여줄 아이디어 개수
   const { data, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery({
     queryKey: ['ideas'],
-    initialPageParam: { page: 1, size: sizePerPage },
+    initialPageParam: { page: 0, size: sizePerPage },
     queryFn: ({ pageParam: { page, size } }) => {
       return getIdeas({ page, size });
     },
 
-    getNextPageParam: (lastPage, allPages) => {
-      const nextPageParam = { page: allPages.length + 1, size: sizePerPage };
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      const nextPageParam = { page: lastPageParam.page + 1, size: sizePerPage };
 
       // 글이 0개 이거나 sizePerPage보다 작으면 마지막페이지로 간주(undefined를 반환)
       return lastPage.length === 0 || lastPage.length < sizePerPage ? undefined : nextPageParam;
