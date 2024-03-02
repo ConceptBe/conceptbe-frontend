e4c8dc00786361a63cad4f37a89854813b1c06c3
semnil5202 <semnil5202@gmail.com>
2024-03-03 02:56:41 +0900

    refactor: 필수 props 에 기본값 제거

--- src/components/HelmetMeta/HelmetMeta.tsx
+++ src/components/HelmetMeta/HelmetMeta.tsx
@@ -5,10 +5,7 @@ interface Props {
   description: string;
 }
 
-const HelmetMeta = ({
-  title = '컨셉비 - 당신의 상상이 현실로!',
-  description = '자유롭고 안전한 아이디어 공유의 장',
-}: Props) => {
+const HelmetMeta = ({ title, description }: Props) => {
   return (
     <Helmet>
       <meta property="og:title" content={title} />
