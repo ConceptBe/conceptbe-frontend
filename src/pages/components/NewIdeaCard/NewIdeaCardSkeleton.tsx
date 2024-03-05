805eddd42d6cd1e2c28da411d50d19d74ff81a20
Youngjin Park <youngjin988@gmail.com>
2024-03-05 21:18:27 +0900

    fix: 페이지 폴더명 대문자로 변경

--- src/pages/components/NewIdeaCard/NewIdeaCardSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-import Skeleton from '../../../components/Skeleton/Skeleton';
-
-const NewIdeaCardSkeleton = () => {
-  return <Skeleton width="100%" height="230px" />;
-};
-
-export default NewIdeaCardSkeleton;
--- src/pages/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export const get999PlusCount = (count: number) => {
-  if (count > 999) {
-    return '999+';
-  }
-  return count;
-};
