51f4fef2311b4dcd265bd7c3adbc33c82b17fe5b
semnil5202 <semnil5202@gmail.com>
2024-03-07 02:19:20 +0900

    feat: 댓글 입력창 클릭시 확장 및 프로필을 보여주는 기능 추가

--- src/pages/FeedDetail/components/Comments.tsx
+++ src/pages/FeedDetail/components/Comments.tsx
@@ -6,14 +6,17 @@ import useCommentsQuery from '../hooks/useCommentsQuery';
 
 interface Props {
   feedId: string;
+  imageUrl: string;
+  nickname: string;
+  skillList: string[];
 }
 
-const Comments = ({ feedId }: Props) => {
+const Comments = ({ feedId, imageUrl, nickname, skillList }: Props) => {
   const { comments } = useCommentsQuery(feedId);
 
   return (
     <Box padding="20px 22px">
-      <WriteComment feedId={feedId} />
+      <WriteComment feedId={feedId} imageUrl={imageUrl} nickname={nickname} skillList={skillList} />
       <Spacer size={20} />
       {comments.map((comment) => (
         <Comment key={comment.content} comment={comment} />
--- src/pages/FeedDetail/components/WriteComment.tsx
+++ src/pages/FeedDetail/components/WriteComment.tsx
@@ -1,23 +1,38 @@
 import styled from '@emotion/styled';
-import { Box, Button, Divider, Flex, theme } from 'concept-be-design-system';
+import { Box, Button, Divider, Flex, Text, theme } from 'concept-be-design-system';
 import { ChangeEvent, useState } from 'react';
 
+import ProfileInfo from '../../../components/ProfileInfo';
 import { useFocusComment } from '../contexts/CommentFocusContext';
 
 interface Props {
   feedId: string;
+  imageUrl: string;
+  nickname: string;
+  skillList: string[];
 }
 
-const WriteComment = ({ feedId }: Props) => {
+const WriteComment = ({ feedId, imageUrl, nickname, skillList }: Props) => {
   const [commentInput, setCommentInput] = useState<string>('');
   const { textareaRef, isFocusComment, focusCommentTextarea, blurCommentTextarea } = useFocusComment();
 
   const onChangeTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setCommentInput(e.target.value);
+    setCommentInput(e.target.value.substring(0, 500));
   };
 
   return (
     <Box position="relative">
+      {isFocusComment && (
+        <Box
+          width="100%"
+          boxSizing="border-box"
+          backgroundColor="bg1"
+          padding="15px 20px 0 20px"
+          borderRadius="6px 6px 0 0"
+        >
+          <ProfileInfo imageUrl={imageUrl} nickname={nickname} skillList={skillList} />
+        </Box>
+      )}
       <Textarea
         isFocus={isFocusComment}
         ref={textareaRef}
@@ -28,32 +43,59 @@ const WriteComment = ({ feedId }: Props) => {
         onFocus={focusCommentTextarea}
         onBlur={blurCommentTextarea}
       />
-      <Divider color="l3" />
-      <Flex>
-        <Button>취소</Button>
-        <Button>등록</Button>
-      </Flex>
+      {isFocusComment && (
+        <Box width="100%" padding="10px 20px" boxSizing="border-box" backgroundColor="bg1" borderRadius="0 0 6px 6px">
+          <Divider color="l3" bottom={10} />
+          <Flex justifyContent="space-between" alignItems="center">
+            <Flex>
+              <Text color="c1">{commentInput.length}</Text>
+              <Text color="ba">/500</Text>
+            </Flex>
+            <Flex width={96} justifyContent="space-between">
+              <CancelButton isGrayOut>취소</CancelButton>
+              <ConfirmButton onClick={() => {}}>등록</ConfirmButton>
+            </Flex>
+          </Flex>
+        </Box>
+      )}
     </Box>
   );
 };
 
 const Textarea = styled.textarea<{ isFocus: boolean }>`
-  border-radius: 6px;
+  display: block;
+  border-radius: ${({ isFocus }) => (isFocus ? '0' : '6px')};
   width: 100%;
-  height: ${({ isFocus }) => isFocus && '209px'};
+  height: ${({ isFocus }) => isFocus && '80px'};
   padding: 10px 20px;
   box-sizing: border-box;
   border: none;
   background-color: ${theme.color.bg1};
-  color: ${theme.color.t};
-  font-style: normal;
-  font-family: SUIT;
-  font-weight: 400;
-  line-height: normal;
+  color: ${theme.color.b4};
+  font-size: ${theme.font.suit14r.fontSize}px;
+  font-weight: ${theme.font.suit14r.fontWeight};
+  line-height: 160%;
 
   ::placeholder {
     color: ${theme.color.ba};
   }
+`;
+
+const CancelButton = styled(Button)`
+  width: 43px;
+  height: 32px;
+  color: ${theme.color.b6};
+  font-size: ${theme.font.suit13m.fontSize}px;
+  font-weight: ${theme.font.suit13m.fontWeight};
+  padding: 6px 10px;
+`;
+
+const ConfirmButton = styled(Button)`
+  width: 43px;
+  height: 32px;
+  font-size: ${theme.font.suit13m.fontSize}px;
+  font-weight: ${theme.font.suit13m.fontWeight};
+  padding: 6px 10px;
 `;
 
 export default WriteComment;
