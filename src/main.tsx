8189083a671775559a6c8383ef352ff29d9b5307
semnil5202 <semnil5202@gmail.com>
2024-03-08 03:47:46 +0900

    feat: Alert를 띄우는 useAlert 훅 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { OverlayProvider } from '@toss/use-overlay';
 import { ConceptBeProvider } from 'concept-be-design-system';
 import { createRoot } from 'react-dom/client';
 
@@ -12,7 +13,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <ConceptBeProvider>
-      <App />
+      <OverlayProvider>
+        <App />
+      </OverlayProvider>
     </ConceptBeProvider>
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
