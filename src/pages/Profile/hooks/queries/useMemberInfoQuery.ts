d9bd743d68955ff55f4a91df3551cd63f6a8ba00
yogjin <youngjin988@gmail.com>
2024-03-10 13:11:31 +0900

    refactor: 유저 정보 받아오는 api: 외부에서 userId 받아오는 것으로 수정

--- src/pages/Profile/hooks/queries/useMemberInfoQuery.ts
+++ src/pages/Profile/hooks/queries/useMemberInfoQuery.ts
@@ -2,16 +2,15 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { http } from '../../../../api/http';
 import { Member } from '../../types';
-import { getUserId } from '../../utils/getUserId';
 
-const getMemberInfo = () => {
-  return http.get<Member>(`/members/${getUserId()}`);
+const getMemberInfo = (userId: number) => {
+  return http.get<Member>(`/members/${userId}`);
 };
 
-export const useMemberInfoQuery = () => {
+export const useMemberInfoQuery = (userId: number) => {
   const { data: memberInfo, ...rest } = useSuspenseQuery({
-    queryKey: ['members', 'detail', getUserId()],
-    queryFn: getMemberInfo,
+    queryKey: ['members', 'detail', userId],
+    queryFn: () => getMemberInfo(userId),
   });
 
   return { ...memberInfo, ...rest };
