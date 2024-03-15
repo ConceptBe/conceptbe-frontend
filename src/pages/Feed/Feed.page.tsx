23ffed1452aebd87eb7d533679a2ecfb702a3811
semnil5202 <semnil5202@gmail.com>
2024-03-15 23:32:35 +0900

    feat: Feed 페이지 상단 글쓰기 버튼 및 '아이디어 적으러 가기'를 통해서 글쓰기 페이지로 이동토록 추가

--- src/pages/Feed/Feed.page.tsx
+++ src/pages/Feed/Feed.page.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
-import { Header, Spacer, Text, theme, SVGHeaderFilter, SVGFeedWrite40 } from 'concept-be-design-system';
+import { Header, Spacer, Text, theme, SVGHeaderFilter, SVGFeedWrite40, Box } from 'concept-be-design-system';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import BestIdeaCardListSection from './components/BestIdeaCardListSection/BestIdeaCardListSection';
 import FilterBottomSheet from './components/FilterBottomSheet/FilterBottomSheet';
@@ -12,6 +13,7 @@ import Logo from '../../layouts/Logo';
 import { useWritingInfoQuery } from '../Write/hooks/queries/useWritingInfoQuery';
 
 const Feed = () => {
+  const navigate = useNavigate();
   const [isFilterBottomSheetOpen, setIsFilterBottomSheetOpen] = useState(false);
   const { branches, purposes, recruitmentPlaces, skillCategoryResponses } = useWritingInfoQuery();
 
@@ -38,7 +40,7 @@ const Feed = () => {
 
       <Wrapper>
         <FeedFixBox>
-          <SVGFeedWrite40 />
+          <SVGFeedWrite40 onClick={() => navigate('/write')} cursor="pointer" />
 
           <Spacer size={27} />
           <FeedFixTextWrapper>
@@ -56,7 +58,9 @@ const Feed = () => {
           </Text>
           <Spacer size={14} />
 
-          <Text font="suit15ra" color="w2">{`아이디어 적으러 가기 >`}</Text>
+          <Box onClick={() => navigate('/write')} cursor="pointer">
+            <Text font="suit15ra" color="w2">{`아이디어 적으러 가기 >`}</Text>
+          </Box>
         </FeedFixBox>
         <IdeaSectionBox>
           <BestIdeaCardListSection />
