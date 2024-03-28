4ede0393d8a7866582c925b02b6ef4cdea545b8a
semnil5202 <semnil5202@gmail.com>
2024-03-28 20:11:34 +0900

    refactor: Card width 지정 및 반응형 로직 추가 및 Card Profile 영역 width -> maxWidth로 변경하여 반응형 대응

--- src/pages/components/NewIdeaCard/compound/Profile/Profile.tsx
+++ src/pages/components/NewIdeaCard/compound/Profile/Profile.tsx
@@ -55,7 +55,7 @@ const Profile = ({ onClickProfile }: Props) => {
           </Text>
           <Spacer size={7} />
 
-          <Flex width={200} wrap="wrap" alignItems="center" gap={4}>
+          <Flex maxWidth={200} wrap="wrap" alignItems="center" gap={4}>
             {skills.map((skill) => (
               <Fragment key={skill}>
                 <FixedSizeText font="suit12r" color="b9">
