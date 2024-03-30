37c0c607c3612819503699405751af42d7ec2f5f
semnil5202 <semnil5202@gmail.com>
2024-03-30 19:57:08 +0900

    feat: 페이지 새로고침 시 세션 스토리지 초기화 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import { FilterProvider } from './pages/Feed/context/filterContext.tsx';
 import router from './router.tsx';
 
 function App() {
+  const clearSessionStorage = () => {
+    sessionStorage.clear();
+  };
+
+  // 새로고침 시 세션 스토리지에 기록된 페이지 스크롤 위치를 모두 삭제합니다.
+  useEffect(() => {
+    window.addEventListener('load', clearSessionStorage);
+
+    return () => window.removeEventListener('load', clearSessionStorage);
+  }, []);
+
   return (
     <FilterProvider>
       <RouterProvider router={router} />
