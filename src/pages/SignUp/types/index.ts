2044fceb26ca18fee9228a42c6cc9dbbe63a7274
semnil5202 <semnil5202@gmail.com>
2024-04-28 01:26:23 +0900

    refactor: 회원가입 페이지 지역 응답값 id로 변경

--- src/pages/SignUp/types/index.ts
+++ src/pages/SignUp/types/index.ts
@@ -17,7 +17,7 @@ export interface PostSignUp {
     level: string;
   }[];
   joinPurposes: number[];
-  livingPlace: string | null;
+  livingPlace: number;
   workingPlace?: string;
   introduction?: string;
   email: string;
