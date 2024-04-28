ac579a7343e45216968a2d7f68e511260421958d
semnil5202 <semnil5202@gmail.com>
2024-04-29 04:23:18 +0900

    fix: 프로필 수정 페이지 지역 put 프로퍼티 오류 수정

--- src/pages/ProfileEdit/ProfileEdit.page.tsx
+++ src/pages/ProfileEdit/ProfileEdit.page.tsx
@@ -88,7 +88,7 @@ const ProfileEdit = () => {
       profileImageUrl: my.profileImageUrl || '',
       skills: selectedSkillDepths.map(({ id, name }) => ({ skillId: id, level: name.split(', ')[1] })),
       joinPurposes: checkboxValue.goal.filter(({ checked }) => checked).map(({ id }) => id),
-      livingPlace: regions.find((place) => place.name === dropdownValue.region)?.id || 1,
+      livingPlaceId: regions.find((place) => place.name === dropdownValue.region)?.id || 1,
       workingPlace: fieldValue.company,
       introduction: fieldValue.intro,
     });
--- src/pages/ProfileEdit/types/index.ts
+++ src/pages/ProfileEdit/types/index.ts
@@ -14,7 +14,7 @@ export interface PutSignUp {
   profileImageUrl: string;
   skills: Omit<ProfileSkill, 'skillName'>[];
   joinPurposes: number[];
-  livingPlace: number;
+  livingPlaceId: number;
   workingPlace?: string;
   introduction?: string;
 }
