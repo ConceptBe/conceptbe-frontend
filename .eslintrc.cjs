cb310b8221c9767cdaef5c5c5c44491fc7e2194e
semnil5202 <semnil5202@gmail.com>
2024-05-02 00:29:59 +0900

    chore: eslint 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,7 @@ module.exports = {
     ],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    'react-refresh/only-export-components': 'off',
   },
   settings: {
     'import/resolver': {
