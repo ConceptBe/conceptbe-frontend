662daa71be1a6d641c3427da74785d641235a67a
이세민 <89172499+semnil5202@users.noreply.github.com>
2024-05-10 06:12:01 +0900

    Refactor/#155: 생성 시간 포멧팅 n분 전, 방금 전 추가 (#156)
    * refactor: 방금 전, n분 전 형식 추가
    
    * docs: n분 전 형식이 반영된 댓글 및 답글 사진 사용
    
    * fix: 닉네임 우측에 콤마 표시 제거

--- src/pages/Feed/utils/formatCommentDate.ts
+++ src/pages/Feed/utils/formatCommentDate.ts
@@ -2,20 +2,29 @@ export function formatCommentDate(date: string) {
   const now = new Date();
   const commentDate = new Date(date);
   const diffTime = Math.abs(now.getTime() - commentDate.getTime());
+  const diffMinutes = Math.floor(diffTime / (1000 * 60));
   const diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+
+  if (diffMinutes < 1) {
+    return '방금 전';
+  }
+
+  if (diffMinutes < 60) {
+    return `${diffMinutes}분 전`;
+  }
 
   if (diffHours < 24) {
     return `${diffHours}시간 전`;
-  } else {
-    const year = commentDate.getFullYear();
-    const month = commentDate.getMonth() + 1; // getMonth()는 0부터 시작하므로 +1
-    const day = commentDate.getDate();
-    const hours = commentDate.getHours();
-    const minutes = commentDate.getMinutes();
-
-    // 숫자가 한 자리일 때 앞에 0을 붙여주는 함수
-    const padZero = (num: number) => num.toString().padStart(2, '0');
-
-    return `${year}.${padZero(month)}.${padZero(day)} ${padZero(hours)}:${padZero(minutes)}`;
   }
+
+  const year = commentDate.getFullYear();
+  const month = commentDate.getMonth() + 1; // getMonth()는 0부터 시작하므로 +1
+  const day = commentDate.getDate();
+  const hours = commentDate.getHours();
+  const minutes = commentDate.getMinutes();
+
+  // 숫자가 한 자리일 때 앞에 0을 붙여주는 함수
+  const padZero = (num: number) => num.toString().padStart(2, '0');
+
+  return `${year}.${padZero(month)}.${padZero(day)} ${padZero(hours)}:${padZero(minutes)}`;
 }
