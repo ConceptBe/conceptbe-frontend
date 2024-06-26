db4ce3d5d0f5f399fe0dca3515b83186f1615613
semnil5202 <semnil5202@gmail.com>
2024-06-26 19:22:55 +0900

    feat: Browser Image 업로드 압축 설정 및 기능 추가

---
+++ src/pages/Write/hooks/useCompressImage.ts
@@ -0,0 +1,28 @@
+import imageCompression from 'browser-image-compression';
+
+const useCompressImage = () => {
+  const compressImage = async (file: File) => {
+    const resizingBlob = await imageCompression(file, {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 750,
+      useWebWorker: true,
+    });
+    const compressedImage = new File([resizingBlob], file.name, {
+      type: file.type,
+    });
+
+    return compressedImage;
+  };
+
+  const compressImages = async (files: FileList) => {
+    const compressedImages = await Promise.all(
+      [...files].map((file) => new Promise((resolve) => resolve(compressImage(file)))),
+    );
+
+    return compressedImages as File[];
+  };
+
+  return { compressImage, compressImages };
+};
+
+export default useCompressImage;
